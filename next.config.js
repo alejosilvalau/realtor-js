3c550354f5dafbd1f2c8f05a46c60b42ef257e41
Alejo Silva <alejosilvalau@Alejos-MBP.fibertel.com.ar>
2022-03-20 18:19:11 -0300

    some changes to build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
-  reactStrictMode: true,
-  images:{
-    domains:['bayut-production.s3.eu-central-1.amazonaws.com']
-},
-}
+const nextConfig = {
+	reactStrictMode: true,
+	images: {
+		domains: ["bayut-production.s3.eu-central-1.amazonaws.com"],
+	},
+	env: {
+		BASE_URL: process.env.BASE_URL,
+	},
+};
+
+module.exports = nextConfig;
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,14 +1,15 @@
 import axios from "axios";
+import endpoint from "../endpoints.config";
 
-export const baseUrl = 'https://bayut.p.rapidapi.com';
+export const baseUrl = "https://bayut.p.rapidapi.com";
 
 export const fetchApi = async (url) => {
-  const { data } = await axios.get((url), {
-    headers: {
-      'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
-    },
-  });
-    
-  return data;
-}
+	const { data } = await axios.get(url, {
+		headers: {
+			"x-rapidapi-host": "bayut.p.rapidapi.com",
+			"x-rapidapi-key": endpoint.ApiKey,
+		},
+	});
+
+	return data;
+};
